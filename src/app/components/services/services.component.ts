6dcdb8cdd029a040d98b4bc69fe69ec81c72dd0f
eciara <ciara.ewelina@gmail.com>
2023-06-15 22:36:48 +0200

    Create the layout of the about section template

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
-
+  cards = [
+    {
+      id: 1,
+      icon: '',
+      title: 'Creative Solutions',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    },
+    {
+      id: 2,
+      icon: '',
+      title: 'Solid Development',
+      desc: 'Sequi aut aliquam, soluta temporibus, magnam provident ducimus ad'
+    },
+    {
+      id: 3,
+      icon: '',
+      title: 'Awesome Designs',
+      desc: 'Incidunt cupiditate adipisci ea facere ratione accusantium qui quas.'
+    },
+    {
+      id: 4,
+      icon: '',
+      title: 'Creative Solutions',
+      desc: 'Ex tempora fugit excepturi fugiat aspernatur tenetur deserunt officiis.'
+    },
+    {
+      id: 5,
+      icon: '',
+      title: 'Solid Development',
+      desc: 'Natus iusto, animi aperiam deleniti facere, itaque quos optio suscipit.'
+    },
+    {
+      id: 6,
+      icon: '',
+      title: 'Awesome Designs',
+      desc: 'Nulla blanditiis aut ut natus enim autem debitis.'
+    },
+  ]
 }

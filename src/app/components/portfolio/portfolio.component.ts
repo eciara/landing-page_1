ba298909277b9d153f6b959fb27d046a4a735bb3
eciara <ciara.ewelina@gmail.com>
2023-06-22 22:02:25 +0200

    Added the initial layout of the Portfolio section

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,50 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  selector: "app-portfolio",
+  templateUrl: "./portfolio.component.html",
+  styleUrls: ["./portfolio.component.scss"],
 })
 export class PortfolioComponent {
-  @Output () attr: any;
+  listButtons = [
+    {
+      title: "All",
+    },
+    {
+      title: "Consulting",
+    },
+    {
+      title: "Social media",
+    },
+    {
+      title: "Blog",
+    },
+  ];
+
+  portfolioGallery = [
+    {
+        id: 1,
+        image: '../../../assets/images/portfolio-1.jpg'
+    },
+    {
+        id: 2,
+        image: '../../../assets/images/portfolio-2.jpg'
+    },
+    {
+        id: 3,
+        image: '../../../assets/images/portfolio-3.jpg'
+    },
+    {
+        id: 4,
+        image: '../../../assets/images/portfolio-4.jpg'
+    },
+    {
+        id: 5,
+        image: '../../../assets/images/portfolio-5.jpg'
+    },
+    {
+        id: 6,
+        image: '../../../assets/images/portfolio-6.jpg'
+    },
+  ]
 }

1a5dfa426e65f28af9eeeac4b3c350641287d3dd
eciara <ciara.ewelina@gmail.com>
2023-06-25 22:43:52 +0200

    Added Pricing section initial layout

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-pricing',
-  templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss']
+  selector: "app-pricing",
+  templateUrl: "./pricing.component.html",
+  styleUrls: ["./pricing.component.scss"],
 })
 export class PricingComponent {
-
+  pricingList = [
+    {
+      id: 1,
+      price: 0,
+      title: "Free Plan",
+      feature1: "150 Lorem",
+      feature2: "20 Lorem Ipsum",
+      feature3: "Lorem Ipsum Dolor Sit.",
+      feature4: "Free Lorem Ipsum",
+      feature5: "Added Lorem Ipsum",
+    },
+    {
+      id: 2,
+      price: 60,
+      title: "Business Plan",
+      feature1: "150 Lorem",
+      feature2: "20 Lorem Ipsum",
+      feature3: "Lorem Ipsum Dolor Sit.",
+      feature4: "Free Lorem Ipsum",
+      feature5: "Added Lorem Ipsum",
+    },
+    {
+      id: 3,
+      price: 96,
+      title: "Enterprise Plan",
+      feature1: "150 Lorem",
+      feature2: "20 Lorem Ipsum",
+      feature3: "Lorem Ipsum Dolor Sit.",
+      feature4: "Free Lorem Ipsum",
+      feature5: "Added Lorem Ipsum",
+    },
+  ];
 }
